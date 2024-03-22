3abc0f092f8e6db94cc36a3acd0c7cf76e379ef5
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-03-22 08:03:36 +0530

    Create Diagonal Sum Binary Tree.py

---
+++ Diagonal Sum Binary Tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Complete the function below
+    def diagonalSum(self, root):
+        from collections import defaultdict
+        diagonal_sum = defaultdict(int)
+        
+        def dfs(node, row, col):
+            if not node:
+                return
+            diagonal_sum[row - col] += node.data
+            if node.left:
+                dfs(node.left, row + 1, col - 1)
+            if node.right:
+                dfs(node.right, row + 1, col + 1)
+        
+        dfs(root, 0, 0)
+        return [value for _, value in sorted(diagonal_sum.items())]
