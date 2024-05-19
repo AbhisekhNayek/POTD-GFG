e4ee483a32839fe494c836db104eecf23abdd3ff
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-05-19 09:36:09 +0530

    Create Find the closest number.py

---
+++ Find the closest number.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+
+    def findClosest(self, n : int, k : int, arr : List[int]) -> int:
+            # code here
+            # Nishant Singh
+            # National Institute of Technology Calicut
+            left,right,ans=0,n-1,0
+            while left<=right:
+                mid=left+(right-left)//2
+                if abs(ans-k)>abs(k-arr[mid]):ans=arr[mid]
+                if abs(ans-k)==abs(k-arr[mid]):ans=max(ans,arr[mid])
+                if arr[mid]>k:right=mid-1
+                else: left=mid+1
+            return ans
