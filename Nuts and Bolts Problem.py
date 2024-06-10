3c38bfb1d524f4365ec6f50aef26feaa20e54414
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-06-10 07:23:05 +0530

    Create Nuts and Bolts Problem.py

---
+++ Nuts and Bolts Problem.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class Solution:
+
+	def matchPairs(self, n, nuts, bolts):
+		# code here
+		x=nuts.sort()
+        y=bolts.sort()
+        return x,y
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        nuts = list(map(str, input().strip().split()))
+        bolts = list(map(str, input().strip().split()))
+        ob = Solution()
+        ob.matchPairs(n, nuts, bolts)
+        for x in nuts:
+            print(x, end=" ")
+        print()
+        for x in bolts:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
